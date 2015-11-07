643cc5ffd816b7ab53bd31805e5ba48e7da60d8d
vishwass <vishwasmys.9491@gmail.com>
2015-11-07 01:02:57 -0500

    working -tested on ccr

--- quac.py
+++ quac.py
@@ -1,7 +1,6 @@
 from pyspark import SparkContext
-sc = SparkContext("local[4]", "quasicliqueEnumeration", pyFiles=['quac.py'])
 
-
+import time
 import itertools
 import math
 import sys,getopt
@@ -10,7 +9,7 @@ import pickle
 from math import *
 from itertools import combinations
 from collections import namedtuple
-from Clique import Clique
+from Cluster import Cluster
 
 changevar =True
 cliques = []  #the list containing gamma quasi cliques
@@ -18,65 +17,102 @@ gamma =0.66 #default value of gamma
 k=3   # default value of k-size of the clique after which gamma should be applied
 
 
+#create each each edge into a clique
+def createinitialClusters(input):
+    initialcluster= Cluster()
+    initialcluster.addedge(input)
+    return (hash(initialcluster),initialcluster)
 
-
-# forms tuples with destination node as the key and edge as the value
-def formSourceDestinationEdgeTuples(d):
-    print "inside formdestinationedgetuples\n",d
-    nodes = d.split(" ")
-    edge = str(nodes[0])+","+str(nodes[1])
-    initialclique1= Clique()
-    initialclique1.update(edge,nodes[0],nodes[1])
-    lst =[]
-    print "nodes[0]",nodes[0]
-    a=(nodes[0],initialclique1)
-    b=(nodes[1],initialclique1)
-    lst.append(a)
-    lst.append(b)
-    return lst
-
-
-def maptest(a):
-    print a
-    return a
-
-
-def cliqueUnion(a,b):
-    c = Clique()
-    c.edges =  a.edges|b.edges
-    c.nodes =  a.nodes|b.nodes
-    return c
 
 def cliquemap(input):
     lister = []
-    for x in input[1].nodes:
-        tup = (x,input[1])
-        lister.append(tup)
-        print "tup",tup[1].nodes
+    for x in input[1].getNodes():
+            tup = (str(x),(set([input]),set([]),False))
+            lister.append(tup)
     return lister
 
-def mergeCliques(input1,input2):
-    global changevar
+def prepareNextIterationInput(input):
+    print "\n\n\niteration output:\n\n\n"
+    print "taskoutput:",input
+    lister = []
+    newclusters = input[1][1]
+    for x in newclusters:
+        lister.append(x)
+    return lister
+
+
+
+
+def findValidCliques(clist1,clist2):
     global gamma
-    global cliques
+    global k
+    print "find valid cliques"    
 
-    newcliq = cliqueUnion(input1,input2)
+    changevar = clist1[2] or clist2[2]
 
-    nnodes = len(newcliq.nodes)
+    temp1set = clist1[0]
+    temp2set = clist2[0]
+    newcandidates =set()
+    for x in temp1set:
+        for y in temp2set:
+            if (x[1].edges.issubset(y[1].edges) or y[1].edges.issubset(x[1].edges)):
+                 continue
+            clust= x[1].unionCluster(y[1])
+            if(clust.isValidClique(gamma,k)):
+                 newcandidates.add(clust.formHashClique())
+                 if changevar == False:
+                     changevar =True
 
-    if len(newcliq.edges) >= gamma * (nnodes*(nnodes-1)/2):
-        changevar =True
-        return newcliq
-
-    # if the criterion is not satisified send an empty clique as it does not form a gamma quasi clique
-    #y = Clique()
-    return input2
-
+    set1 = temp1set.union(temp2set)
+    set2 = newcandidates.union(clist1[1])
+    set2 = set2.union(clist2[1])
+    set2 = removesubsets(set2)
+    result = (set1,set2,changevar)
+    return result
 
 
+# Given a set of clusters ,removes all the clusters which are subsets of other clusters in the input set
+def removesubsets(inputset):
+    subsets =set()
+    for x in inputset:
+        for y in inputset:
+            if x[1] != y[1]:
+                if x[1].isSubsetOf(y[1]):
+                    subsets.add(x)
+    
+    return inputset-subsets
 
 
 
+def combineTheReducers(input1,input2):
+
+    rlist1 =set()
+    rlist2 =set()
+    #tree reduce remove redundant clusters
+    for cluster1 in input1[1][0]:
+        for cluster2 in input2[1][0]:
+            if(cluster1[1].isSubsetOf(cluster2[1])):
+                rlist1.add(cluster1)
+            elif (cluster2[1].isSubsetOf(cluster1[1])):
+                rlist2.add(cluster2)
+
+    for x in rlist1:
+        input1[1][0].remove(x)
+    for y in rlist2:
+        input2[1][0].remove(y)
+    set1 = input1[1][0].union(input2[1][0])
+    set2 = input1[1][0].union(input2[1][0])
+
+    change = input1[1][2] or input2[1][2]
+
+    return (str(0),(set1,set2,change))
+
+
+
+def returnChangevar(input1,input2):
+
+    change = input1[1][2] or input2[1][2]
+    return (str(0),(set(),set(),change))
 
 
 
@@ -108,31 +144,36 @@ if __name__ == "__main__":
     print 'gamma value =',gamma
     print 'k value =', k
 
+    beg = time.time()
 
-    #creating adjacency list
-    cliques= sc.textFile("edges").flatMap(formSourceDestinationEdgeTuples).reduceByKey(lambda a,b:cliqueUnion(a,b)).collect()
+    sc = SparkContext(appName="quasicliqueEnumeration", pyFiles=['Cluster.py'])
 
-    print "initial cliques"
-    for x in cliques :
-        print x[1].edges
 
-    cliques = sc.parallelize(cliques)
+    #creating initial set of clusters
+    clusters= sc.textFile("edges.g3").map(createinitialClusters)
 
+    iteration = 0
     while(changevar):
         changevar = False
-        cliques = cliques.flatMap(cliquemap).reduceByKey(mergeCliques)
+        clusters = clusters.flatMap(cliquemap).reduceByKey(findValidCliques)
+        changevar = clusters.reduce(returnChangevar)[1][2]
 
+        iteration = iteration+1
+        print "/////////iteration number:",iteration,"/////////"
+        print "changevar:",changevar
+        if(changevar):
+            clusters = clusters.flatMap(prepareNextIterationInput)
+            
 
-    cliques = cliques.collect()
+       
+    clusters =clusters.treeReduce(combineTheReducers)
 
+    print '\nlist of gamma quasi cliques with gamma=',gamma,'k=',k,"iterations=",iteration
+    print clusters
+    for x in clusters[1][1]:
+            x[1].clusterPrint()
 
-
-
-    print '\nlist of gamma quasi cliques with gamma=',gamma,'k=',k
-    for x in cliques :
-        print "clique",x
-        print x[1].edges
-        print '\n'
-
-
+    sc.stop()
   
+    end = time.time();
+    print "time =", (end - beg)
